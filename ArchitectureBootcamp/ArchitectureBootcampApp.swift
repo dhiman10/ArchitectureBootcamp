89e8b6805015bcdcd45c2a454859b2e5425d5388
dhiman10 <dhimandas10.sec@gmail.com>
2025-09-13 15:40:59 +0200

    Initial Commit

---
+++ ArchitectureBootcamp/ArchitectureBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArchitectureBootcampApp.swift
+//  ArchitectureBootcamp
+//
+//  Created by Dhiman Das on 13.09.25.
+//
+
+import SwiftUI
+
+@main
+struct ArchitectureBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
